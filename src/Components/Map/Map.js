22ba9659a52203dd914c08c1255bf7471440d1c1
DmitriyHerasymenko <dmitriy.herasymenko@482.solutions>
2021-04-29 09:16:37 +0300

    rewrite class component to functional component

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -2,92 +2,95 @@ import React from 'react';
 import { withGoogleMap, withScriptjs, GoogleMap, Marker, Polyline } from 'react-google-maps';
 import { getRequest } from '../Request';
 
-function getCoordinatesWithDistance(coordinates, i, array) {
-    if (i === 0) {
-        return { ...coordinates, distance: 0 };
-    }
-    const { lat: lat1, lng: lng1 } = coordinates;
-    const latLong1 = new window.google.maps.LatLng(lat1, lng1);
+const Map = () => {
 
-    const { lat: lat2, lng: lng2 } = array[0];
-    const latLong2 = new window.google.maps.LatLng(lat2, lng2);
+    const [ path, setPath ] = React.useState([]);
+    const [ progress, setProgress ] = React.useState([]);
+    const [ velocity, setVelocity ] = React.useState(5);
+    const [ initialDate, setInitialDate ] = React.useState(new Date());
 
-    const distance = window.google.maps.geometry.spherical.computeDistanceBetween(latLong1, latLong2);
+    React.useEffect(() => {
 
-    return { ...coordinates, distance };
-}
+        const getCoordinatesWithDistance = (coordinates, i, array) => {
+            if (i === 0) {
+                return { ...coordinates, distance: 0 };
+            }
+            const { lat: lat1, lng: lng1 } = coordinates;
+            const latLong1 = new window.google.maps.LatLng(lat1, lng1);
 
-class Map extends React.Component {
-    state = {
-        path: [],
-        progress: [],
-        velocity: 5,
-        initialDate: new Date(),
-    };
+            const { lat: lat2, lng: lng2 } = array[0];
+            const latLong2 = new window.google.maps.LatLng(lat2, lng2);
 
-    getDistance = () => {
-        const differentInTime = (new Date() - this.state.initialDate) / 1000;
-        return differentInTime * this.state.velocity;
-    };
+            const distance = window.google.maps.geometry.spherical.computeDistanceBetween(latLong1, latLong2);
 
-    componentWillMount = async () => {
-        this.interval = window.setInterval(this.moveObject, 1000);
-        const data = await getRequest();
-        const modPath = data.map(getCoordinatesWithDistance);
-        this.setState({ path: modPath });
-        console.log('state', this.state);
-    };
-
-    componentWillUnmount = () => {
-        window.clearInterval(this.interval);
-    };
-
-    moveObject = () => {
-        const distance = this.getDistance();
-        if (!distance) {
-            return;
+            return { ...coordinates, distance };
         }
 
-        let progress = this.state.path.filter((coordinates) => coordinates.distance < distance);
+        const moveObject = () => {
 
-        const nextLine = this.state.path.find((coordinates) => coordinates.distance > distance);
+            const getDistance = () => {
+                const differentInTime = (new Date() -initialDate) / 1000;
+                return differentInTime * velocity;
+            };
 
-        if (!nextLine) {
-            this.setState({ progress });
-            return;
+            const distance = getDistance();
+            if (!distance) {
+                return;
+            }
+
+            let curProgress = path.filter((coordinates) => coordinates.distance < distance);
+            const nextLine = path.find((coordinates) => coordinates.distance > distance);
+
+            if (!nextLine) {
+                setProgress(curProgress);
+                return;
+            }
+            const lastLine = curProgress[curProgress.length - 1];
+
+            const lastLineLatLng = new window.google.maps.LatLng(lastLine.lat, lastLine.lng);
+
+            const nextLineLatLng = new window.google.maps.LatLng(nextLine.lat, nextLine.lng);
+
+            const totalDistance = nextLine.distance - lastLine.distance;
+            const percentage = (distance - lastLine.distance) / totalDistance;
+
+            const position = window.google.maps.geometry.spherical.interpolate(lastLineLatLng, nextLineLatLng, percentage);
+
+            curProgress = curProgress.concat(position);
+            setProgress(curProgress);
+        };
+
+        async function fetchData() {
+            const data = await getRequest();
+            const modPath = data.map(getCoordinatesWithDistance);
+            setPath(modPath);
         }
-        const lastLine = progress[progress.length - 1];
 
-        const lastLineLatLng = new window.google.maps.LatLng(lastLine.lat, lastLine.lng);
+        const interval = window.setInterval(moveObject, 1000);
 
-        const nextLineLatLng = new window.google.maps.LatLng(nextLine.lat, nextLine.lng);
+        if (path.length === 0) {
+            fetchData();
+        }
 
-        const totalDistance = nextLine.distance - lastLine.distance;
-        const percentage = (distance - lastLine.distance) / totalDistance;
+        return () => {
+            window.clearInterval(interval);
+        }
+    },[initialDate, velocity, path]);
 
-        const position = window.google.maps.geometry.spherical.interpolate(lastLineLatLng, nextLineLatLng, percentage);
-
-        progress = progress.concat(position);
-        this.setState({ progress });
-    };
-
-    render = () => {
-        return (
-            <GoogleMap defaultZoom={16} defaultCenter={{ lat: 48.750488, lng: 30.21979 }}>
-                <Polyline path={this.state.progress} options={{ strokeColor: '#FF0000 ' }} />
-                <Marker position={this.state.progress[this.state.progress.length - 1]} />
-            </GoogleMap>
-        );
-    };
+    return  <GoogleMap defaultZoom={16} defaultCenter={{ lat: 48.750488, lng: 30.21979 }}>
+        <Polyline path={progress} options={{ strokeColor: '#FF0000 ' }} />
+        <Marker position={progress[progress.length - 1]} />
+    </GoogleMap>;
 }
 
 const MapComponent = withScriptjs(withGoogleMap(Map));
 
-export default () => (
+const GMC = () => (
     <MapComponent
         googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px`, width: '500px' }} />}
         mapElement={<div style={{ height: `100%` }} />}
     />
-);
+)
+export default GMC;
